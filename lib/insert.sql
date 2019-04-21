b5978afe2644381d36154e226103f77b8cda38cc
Ming Duan <11203duan@gmail.com>
2019-04-21 19:53:08 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 8, "Male", "Brown", "Mild", "1");
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 1, "Male", "Brown", "Mild", "1");
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 6, "Female", "Black", "Mild", "1");
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 12, "Male", "Brown", "Wild", "1");
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 2, "Female", "White", "Mild", "1");
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 5, "Male", "Brown", "Mild", "0");
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 1, "Female", "Black", "Mild", "1");
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (null, 2, "Male", "Brown", "Violent", "1");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 8, "M", "Brown", "Mild", "1");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 1, "M", "Brown", "Mild", "1");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 6, "F", "Black", "Mild", "1");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 12, "M", "Brown", "Wild", "1");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 2, "F", "White", "Mild", "1");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 5, "M", "Brown", "Mild", "0");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 1, "F", "Black", "Mild", "1");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (null, 2, "M", "Brown", "Violent", "1");
