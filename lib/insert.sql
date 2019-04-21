14303f2d9eb92ae560c7dcd03b314a6269598377
Ming Duan <11203duan@gmail.com>
2019-04-21 19:56:36 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -3,6 +3,6 @@ INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy"
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 6, "F", "Black", "Mild", "1");
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 12, "M", "Brown", "Wild", "1");
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 2, "F", "White", "Mild", "1");
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 5, "M", "Brown", "Mild", "0");
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 1, "F", "Black", "goofy", "1");
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (null, 2, "M", "Brown", "Violent", "1");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 5, "M", "dark brown", "Mild", "0");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 1, "F", "dark brown", "goofy", "1");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (null, 2, "M", "dark brown", "Violent", "1");
