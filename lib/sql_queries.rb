3c072869b6de66f9d17a0858d0756ce8bb9f40f5
Ming Duan <11203duan@gmail.com>
2019-04-21 19:45:16 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,5 +1,5 @@
 def selects_all_female_bears_return_name_and_age
-  "SELECT name, age FROM bears WHERE gender = "Female";"
+  "SELECT name, age FROM bears WHERE gender = 'Female';"
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
@@ -19,11 +19,11 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "COUNT(*) FROM bears WHERE color = "White";"
+  "COUNT(*) FROM bears WHERE color = 'White';"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  "COUNT(*) FROM bears WHERE temperaments = "Violent";"
+  "COUNT(*) FROM bears WHERE temperaments = 'Violent';"
 end
 
 def selects_bear_that_killed_Tim
